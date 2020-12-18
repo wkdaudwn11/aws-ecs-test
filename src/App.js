f2ff76873bf1333efe2fa11dce6c06097b53a5b1
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-12-18 11:31:14 +0900

    Code update

--- src/App.js
+++ src/App.js
@@ -6,20 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          host: {window.location.host}
-          <br />
-          hostname: {window.location.hostname}
-          <br />
-          deploy #2
+          deploy #3
         </a>
       </header>
     </div>
